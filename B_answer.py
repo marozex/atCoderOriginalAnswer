a9314130809a741f53067d7f7b60cfe7d21c445c
島倉健麿 <shimakura0213@gmail.com>
2019-07-15 16:15:34 +0900

    047追加

--- B_answer.py
+++ B_answer.py
@@ -14,3 +14,30 @@ for a in al:
         print("No")
         exit()
 print("Yes")
+
+
+# abc046
+N, K = map(int, input().split())
+print(K * (K-1)**(N-1))
+
+# abc047
+x, y, n = map(int, input().split())
+input = [list(map(int, input().split())) for _ in range(n)] 
+xmax = x
+xmin = 0
+ymax = y
+ymin = 0
+for i in input:
+    type = i[2]
+    if type == 1 and (i[0] > xmin):
+        xmin = i[0]
+    elif type == 2 and (i[0] < xmax):
+        xmax = i[0]
+    elif type == 3 and (i[1] > ymin):
+        ymin = i[1]
+    elif type == 4 and (i[1] < ymax):
+        ymax = i[1]
+if (xmax > xmin) and (ymax > ymin):
+    print((xmax - xmin)*(ymax - ymin))
+else:
+    print(0)
