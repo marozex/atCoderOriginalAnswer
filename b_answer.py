53b3edd6660f25598954acf9a50b14e0f541063e
島倉健麿 <shimakura0213@gmail.com>
2019-07-22 23:49:46 +0900

    abc054追加

--- b_answer.py
+++ b_answer.py
@@ -110,3 +110,17 @@ st =input()
 ai = st.find('A')
 zi = st.rfind('Z')
 print(zi - ai + 1)
+
+# abc054b
+import sys
+n, m = map(int, input().split())
+a_list = [input() for _ in range(n)]
+b_list = [input() for _ in range(m)]
+
+for i in range(n-m+1):
+    for j in range(n-m+1):
+        new_list = [a_list[k][j:j+m] for k in range(i,i+m)]
+        if(new_list == b_list):
+            print('Yes')
+            sys.exit()
+print('No')
