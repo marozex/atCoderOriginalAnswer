8daa730d26868583af30dc832d6ad04be8ddeb77
島倉健麿 <shimakura0213@gmail.com>
2019-07-28 21:41:55 +0900

    057c追加

--- c_anwer.py
+++ c_anwer.py
@@ -8,3 +8,19 @@ left = right.replace("R", "L")
 first = up + right + down + left
 second = "LU" + up + right + "RDRD" + down + left + "LU"
 print(first+second)
+
+# abc054cは保留
+
+# abc057c
+import math
+n = int(input())
+s = math.ceil(math.sqrt(n))
+tmp = []
+for i in range(2,s+1):
+    if n%i == 0:
+        m = max(i, n//i)
+        tmp.append(int(math.log10(m))+1)
+if len(tmp) == 0:
+    print(int(math.log10(n))+1)
+else:
+    print(min(tmp))
